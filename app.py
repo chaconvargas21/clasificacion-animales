e5584943d420d746935b42e76596ca9417d4c2ec
Alex De la cruz Quispe <my.wab.alex@gmail.com>
2020-10-28 00:34:48 -0500

    archivos agregados

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/',methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    return render_template("menu.html")
+
+@app.route("/predictor",methods=["POST"])
+def predictor():
+    return render_template("predictor.html")
+
+@app.route("/clasificador",methods=["POST"])
+def clasificador():
+    return render_template("")
+
 if __name__ == "__main__" :
     app.run(debug=True)
